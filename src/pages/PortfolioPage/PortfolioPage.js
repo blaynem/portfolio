4aad0bca9696e7455d9aa460a30b1f015126b330
Blayne Marjama <Blayne.marjama@gmail.com>
2017-02-04 18:49:29 -0800

    added the store app im working on

--- src/pages/PortfolioPage/PortfolioPage.js
+++ src/pages/PortfolioPage/PortfolioPage.js
@@ -14,6 +14,16 @@ export default class PortfolioPage extends Component {
 		const descripStyle = {
 			color: "white"	
 		}
+		const storepics = {
+		    position: "relative",
+		    padding: 0,
+		    marginBottom: "15px",
+		    height: "600px",
+		    backgroundPosition: "50% 50%",
+		    backgroundRepeat: "no-repeat",
+		    backgroundSize: "contain",
+		    backgroundImage: "url(images/storepic.png)"
+		}
 		const jaredPics = {
 		    position: "relative",
 		    padding: 0,
@@ -36,7 +46,41 @@ export default class PortfolioPage extends Component {
 		}
 		return (
 			<div style={{paddingTop:"55px"}}>
-				<div style={{padding:0}} className="col-xs-12">
+				<div style={{paddingTop:"15px", paddingBottom:"15px"}} className="col-xs-12">
+					<a href="http://blaynestoretest.surge.sh/" target="_blank">
+						<div style={storepics} className="col-md-6"></div>
+					</a>
+					<div className="col-md-6">
+						<div style={descripDivStyle} className="col-xs-12">
+							<h1 style={headerStyle}>Web Store App (Current Project)</h1>
+							<h3 style={descripStyle}>Began creating a Web Store App project this weekend (2/4/17) because I really wanted to solidify
+							my knowledge of Redux, and work with ES6 more. I'll be doing constant push updates to the site, so make sure you come check
+							back later and see the updates.</h3>
+							<div className="col-sm-6">>
+							<h3 style={learnedStyles}>Things I learned:</h3>
+							<ul style={ulStyle}>
+								<li><span style={liStyles}>Mocking API data</span></li>
+								<li><span style={liStyles}>Reusability (I repeat that a lot, but React is awesome like that)</span></li>
+								<li><span style={liStyles}>Redux/Stores</span></li>
+							</ul>
+							</div>
+							<div className="col-sm-6">>
+							<h3 style={learnedStyles}>Obstacles:</h3>
+							<ul style={ulStyle}>
+								<li><span style={liStyles}>Caching</span></li>
+								<li><span style={liStyles}>Figuring out how to get cart working correctly</span></li>
+								<li><span style={liStyles}>Figuring out Check-out process</span></li>
+								<li><span style={liStyles}>Had trouble figuring out exactly how to get the right data to places, but ultimately figured it out.</span></li>
+							</ul>
+							</div>
+							<h3 style={descripStyle}>You can view the source code
+								<a style={{color:(this.props.theColor)}}
+								   href="https://github.com/blaynem/store-prototype"
+								   target="_blank"> here</a></h3>
+						</div>
+					</div>
+				</div>
+				<div style={{paddingTop:"15px", paddingBottom:"15px"}} className="col-xs-12">
 					<div className="col-md-6">
 						<div style={descripDivStyle} className="col-xs-12">
 							<h1 style={headerStyle}>Weather App w/ Api</h1>
