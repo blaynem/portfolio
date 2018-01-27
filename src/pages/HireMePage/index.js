7a645819804547a2f8d350c070f5b72ea3244648
Blayne Marjama <blayne.marjama@gmail.com>
2018-01-27 14:34:01 -0800

    fixed hire me page

--- src/pages/HireMePage/index.js
+++ src/pages/HireMePage/index.js
@@ -4,34 +4,34 @@ import ConnectLinks from '../../components/ConnectLinks';
 
 export default class HireMePage extends Component {
 	render() {
-
-		var container = {
+		const { theColor } = this.props
+		const container = {
 			overflowX: "hidden",
 			display: "inline-block",
 			background: "rgba(0,0,0,0.6)",
 			marginTop: "5vh"
 		}
 
-		var whoops = {
+		const whoops = {
 			fontSize: "4em",
 			textAlign: "center",
-			color:(this.props.theColor)
+			color:theColor
 		}
 
-		var paraStyle = {
+		const paraStyle = {
 			color: "white",
 			textAlign: "left",
 			margin: "0 0 10px 0",
 			fontSize: "1.5em"
 		}
 
-		var pizzaz = {
-			color: (this.props.theColor)
+		const pizzaz = {
+			color: theColor
 		}
 
-		var arrowStyle = {
+		const arrowStyle = {
 			margin: 0,
-			color: (this.props.theColor),
+			color: theColor,
 			textAlign: "center",
 			fontSize: "30px"
 		}
@@ -45,15 +45,13 @@ export default class HireMePage extends Component {
 				</div>
 				<div className="row">
 					<div className="col-lg-offset-1 col-lg-10">
-						<h3 style={paraStyle}>I prefer working on <span style={pizzaz}>Front-End Development</span> but enjoy
-						<span style={pizzaz}>QA Testing</span> as well.</h3>
+						<h3 style={paraStyle}>I prefer working on with <span style={pizzaz}>React</span> and <span style={pizzaz}>Redux</span> on the Frontend.</h3> 
 						<h3 style={paraStyle}>I'm currently enjoying the beautiful nature scenery of <span style={pizzaz}>Portland, OR</span>,
-							but am open to relocating or potentially working remotely. If you'd like to go over a potential opportunity or remote
-							position feel free to reach out via email.</h3>
+							but am open to relocating or working remotely. If you'd like to go over a potential opportunity feel free to reach out via email.</h3>
 						<h3 style={paraStyle}><span style={pizzaz}>Email: </span><a href="mailto:Blayne.Marjama@gmail.com" style={{color:"white"}}>Blayne.Marjama@gmail.com</a></h3>
 						<h3 style={paraStyle}>I can also be reached at one of the social media links below.</h3>
 						<h3 style={arrowStyle}><i className="glyphicon glyphicon-chevron-down"></i></h3>
-						<div style={{textAlign:"center"}}><ConnectLinks theColor={this.props.theColor}/></div>
+						<div style={{textAlign:"center", marginBottom: "20px" }}><ConnectLinks theColor={theColor}/></div>
 					</div>
 				</div>
 			</div>
