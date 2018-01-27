6ec7b2efdfee2fcc6c32e5b3e10c683e74dd396f
Blayne Marjama <blayne.marjama@gmail.com>
2018-01-27 12:54:15 -0800

    moving to new CRA version -- copied over the mess

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from "react-router-dom";
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from "react-router-dom";
 
-// import App from './App';
-// import Routes from './routes';
+import App from './App';
+import Routes from './routes';
 
 import './index.css';
 
 ReactDOM.render(
-  // <Router>
-  //   <App />
-  // </Router>
-  <div>Test</div>,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
+registerServiceWorker();
