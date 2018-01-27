5adc037f53bdb0fbfe048ede3fe21457357a9309
Blayne Marjama <blayne.marjama@gmail.com>
2018-01-27 13:21:13 -0800

    finsihed adding all pages, neat!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from './pages/AboutPage';
 import Portfolio from './pages/PortfolioPage';
+import HireMePage from './pages/HireMePage';
+import Testimonials from './pages/TestimonialsPage';
+import NotFound from './pages/NotFound';
+
 import NavBar from "./components/NavBar";
 import Footer from './components/Footer';
 
@@ -120,6 +124,9 @@ export default class App extends Component {
               <Route exact path="/" component={() => <HomePage theColor={theColor} />}/>
               <Route exact path="/about" component={() => <AboutPage theColor={theColor} />}/>
               <Route exact path="/portfolio" component={() => <Portfolio theColor={theColor} />}/>
+              <Route exact path="/testimonials" component={() => <Testimonials theColor={theColor} />}/>
+              <Route exact path="/hire-me" component={() => <HireMePage theColor={theColor} />}/>
+              <Route component={() => <NotFound theColor={theColor} />}/>
             </Switch>
           </div>
           <div>
